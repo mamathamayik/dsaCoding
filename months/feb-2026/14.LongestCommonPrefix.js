d1983d4a0daecc1e30b7a9c1b581f99b0267a664
mamathamayi <mamathamayi_k@epam.com>
2026-02-16 12:05:36 +0530

    completed

---
+++ months/feb-2026/14.LongestCommonPrefix.js
@@ -0,0 +1,13 @@
+
+function LongestPrefix(strs){
+    for(let i=0;i<strs[0].length;i++){
+        let char = strs[0][i];
+        for(let j=1;j<strs.length;j++){
+            if(i>=strs[j].length || strs[j][i]!==char)
+                return strs[0].slice(0,i);
+        }
+    }
+    return strs[0];
+}
+
+console.log(LongestPrefix(["flower","flow","flight"]));
